0c0d63ad146a3f28df35d9e368f781ec5d589fb4
SimoneSaramondi <simo98sara@gmail.com>
2021-04-08 15:21:15 +0200

    prima parte

---
+++ myscript.js
@@ -0,0 +1,29 @@
+var chilometriDaPercorrere = prompt("Quanti chilometri devi percorrere?");
+console.log(chilometriDaPercorrere);
+
+var eta = prompt("Inserisci la tua età");
+console.log(eta);
+
+var bigliettoAlChilometro = 0.21;
+
+var scontoMinorenni = 0.2;
+
+var scontoAnziani = 0.4;
+
+var costoViaggio = bigliettoAlChilometro * chilometriDaPercorrere;
+
+var costoViaggioMinorenni = costoViaggio * scontoMinorenni;
+
+var costoViaggioAnziani = costoViaggio * scontoAnziani;
+
+if(Number.isNaN(chilometriDaPercorrere)){
+    console.log("Dati inseriti non validi");
+}
+
+if(eta >= 18 && eta <= 65){
+    console.log("Il prezzo del biglietto è di " + costoViaggio + " Euro");
+} else if(eta < 18){
+    console.log("Il prezzo del biglietto è di " + costoViaggioMinorenni.toFixed(2) + " Euro");
+} else {
+    console.log("Il prezzo del biglietto è di " + costoViaggioAnziani.toFixed(2) + " Euro");
+}
